e00b5be9f13aab4f420a8e4cd6d60f1fdd026292
zhangchunsheng <zhangchunsheng423@gmail.com>
2020-07-13 13:16:47 +0800

    update

---
+++ go-grpc-example/server/simple_server/server.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"context"
+	"github.com/grpc-ecosystem/go-grpc-middleware"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"log"
+	"net"
+	"runtime/debug"
+
+	"github.com/EDDYCJY/go-grpc-example/pkg/gtls"
+	pb "github.com/EDDYCJY/go-grpc-example/proto"
+)
+
+type SearchService struct{}
+
+func (s *SearchService) Search(ctx context.Context, r *pb.SearchRequest) (*pb.SearchResponse, error) {
+	if ctx.Err() == context.Canceled {
+		return nil, status.Errorf(codes.Canceled, "searchService.Search canceled")
+	}
+
+	return &pb.SearchResponse{Response: r.GetRequest() + " Server"}, nil
+}
+
+const PORT = "9001"
+
+func main() {
+	tlsServer := gtls.Server{
+		CaFile:   "../../conf/ca.pem",
+		CertFile: "../../conf/server/server.pem",
+		KeyFile:  "../../conf/server/server.key",
+	}
+	c, err := tlsServer.GetCredentialsByCA()
+	if err != nil {
+		log.Fatalf("GetTLSCredentialsByCA err: %v", err)
+	}
+
+	opts := []grpc.ServerOption{
+		grpc.Creds(c),
+		grpc_middleware.WithUnaryServerChain(
+			RecoveryInterceptor,
+			LoggingInterceptor,
+		),
+	}
+
+	server := grpc.NewServer(opts...)
+	pb.RegisterSearchServiceServer(server, &SearchService{})
+
+	lis, err := net.Listen("tcp", ":"+PORT)
+	if err != nil {
+		log.Fatalf("net.Listen err: %v", err)
+	}
+
+	server.Serve(lis)
+}
+
+func LoggingInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	log.Printf("gRPC method: %s, %v", info.FullMethod, req)
+	resp, err := handler(ctx, req)
+	log.Printf("gRPC method: %s, %v", info.FullMethod, resp)
+	return resp, err
+}
+
+func RecoveryInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+	defer func() {
+		if e := recover(); e != nil {
+			debug.PrintStack()
+			err = status.Errorf(codes.Internal, "Panic err: %v", e)
+		}
+	}()
+
+	return handler(ctx, req)
+}
