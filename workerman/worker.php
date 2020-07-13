45ff37324618e37a0a05e534312952592c5e7e23
zhangchunsheng <zhangchunsheng423@gmail.com>
2020-07-13 15:03:37 +0800

    update

---
+++ workerman/worker.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: peterzhang
+ * Date: 2020/7/13
+ * Time: 3:03 PM
+ */
+//composer require workerman/workerman
+//composer require react/http-client:^0.5.9
+
+require_once __DIR__ . '/vendor/autoload.php';
+use Workerman\Worker;
+use Workerman\Connection\AsyncTcpConnection;
+use Amp\Artax\Response;
+
+$http_worker = new Worker("http://0.0.0.0:8082");
+
+$http_worker->count = 1; // 只开一个进程
+
+$http_worker->onMessage = function($connection, $host) {
+    echo 1;
+    $data = file_get_contents('http://localhost:8081');
+    $connection->send($data);
+};
+
+Worker::runAll();
