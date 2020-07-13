d635200efc38e3b5ddeb44189da0fd92d7b0c9e6
zhangchunsheng <zhangchunsheng423@gmail.com>
2020-07-13 15:19:38 +0800

    update

--- workerman/worker.php
+++ workerman/worker.php
@@ -23,10 +23,26 @@ $http_worker = new Worker("http://0.0.0.0:8083");
 
 $http_worker->count = 1; // 只开一个进程
 
-$http_worker->onMessage = function($connection, $host) {
+/*$http_worker->onMessage = function($connection, $host) {
     echo 1;
     $data = file_get_contents('http://localhost:8082');
     $connection->send($data);
+};*/
+
+$http_worker->onMessage = function($connection, $host) {
+    echo 1;
+    $loop    = Worker::getEventLoop();
+    $client  = new \React\HttpClient\Client($loop);
+    $request = $client->request('GET', 'http://localhost:8081');
+    $request->on('error', function(Exception $e) use ($connection) {
+        $connection->send($e);
+    });
+    $request->on('response', function ($response) use ($connection) {
+        $response->on('data', function ($data) use ($connection) {
+            $connection->send($data);
+        });
+    });
+    $request->end();
 };
 
 Worker::runAll();
