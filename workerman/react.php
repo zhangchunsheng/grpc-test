6baaa72afaeba80cf0d0c5dde3c48a73854057bd
zhangchunsheng <zhangchunsheng423@gmail.com>
2020-07-13 15:23:15 +0800

    update

---
+++ workerman/react.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: peterzhang
+ * Date: 2020/7/13
+ * Time: 3:22 PM
+ */
+//php react.php
+//telnet 127.0.0.1 8082
+$context = stream_context_create();
+$socket = stream_socket_server('tcp://0.0.0.0:8082', $errno, $errmsg, STREAM_SERVER_BIND | STREAM_SERVER_LISTEN, $context); // 注册一个 fd（file descriptor)
+
+function react($socket){
+    $new_socket = stream_socket_accept($socket, 0, $remote_address);
+    echo 1;
+}
+
+$eventBase = new EventBase();
+$event = new Event($eventBase, $socket, Event::READ | Event::PERSIST, 'react', $socket); // 注册一个事件，检测 fd 有没有写入内容
+$event->add();
+$eventBase->loop(); // 开始循环
