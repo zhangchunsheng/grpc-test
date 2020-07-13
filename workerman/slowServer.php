d30bb2f30e80d3cd4752661ce816d985827c53a4
zhangchunsheng <zhangchunsheng423@gmail.com>
2020-07-13 14:58:23 +0800

    update

--- workerman/slowServer.php
+++ workerman/slowServer.php
@@ -5,6 +5,7 @@
  * Date: 2020/7/13
  * Time: 2:55 PM
  */
-// php -S localhost:8081 slowServer.php
+// php -S localhost:8082 slowServer.php
+// curl 'localhost:8082'
 sleep(5); // 5秒后才能返回请求
 echo 'done';
