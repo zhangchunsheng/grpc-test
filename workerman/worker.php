cfc31e8c1e1b6fe9b83408c28e8d561fa2418a34
zhangchunsheng <zhangchunsheng423@gmail.com>
2020-07-13 15:15:21 +0800

    update

--- workerman/worker.php
+++ workerman/worker.php
@@ -9,18 +9,23 @@
 //composer require react/http-client:^0.5.9
 
 //php worker.php start
+/**
+ * sudo yum install php74-php-posix
+ * php74-php-process
+ * curl 'http://localhost:8083'
+ */
 require_once __DIR__ . '/vendor/autoload.php';
 use Workerman\Worker;
 use Workerman\Connection\AsyncTcpConnection;
 use Amp\Artax\Response;
 
-$http_worker = new Worker("http://0.0.0.0:8082");
+$http_worker = new Worker("http://0.0.0.0:8083");
 
 $http_worker->count = 1; // 只开一个进程
 
 $http_worker->onMessage = function($connection, $host) {
     echo 1;
-    $data = file_get_contents('http://localhost:8081');
+    $data = file_get_contents('http://localhost:8082');
     $connection->send($data);
 };
 
